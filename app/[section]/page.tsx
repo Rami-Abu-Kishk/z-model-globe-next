548531caff2d900b3ba433b147c60145e77d2cc8
Deya-Darweesh-ITC <deya@itc-international.net>
2026-04-16 11:55:18 +0300

    routes implemented and npm run fixed

---
+++ app/[section]/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { useZModelStore, ModuleId } from "@/lib/store";
+import Home from "@/app/page";
+
+export default function DeepLinkedPage() {
+  const params = useParams();
+  const setActiveModule = useZModelStore((state) => state.setActiveModule);
+  const setViewState = useZModelStore((state) => state.setViewState);
+  const setFocusedCardId = useZModelStore((state) => state.setFocusedCardId);
+
+  useEffect(() => {
+    const section = params.section as string;
+    const validModules: ModuleId[] = [
+      "economy",
+      "investment",
+      "political",
+      "media",
+      "research",
+      "groups",
+      "masterObserver",
+      "abuDhabiGov",
+      "calendar",
+    ];
+
+    if (validModules.includes(section as ModuleId)) {
+      // Small delay to ensure the main component is hydrated
+      const timer = setTimeout(() => {
+        setActiveModule(section as ModuleId);
+        setViewState("CARD_FOCUS");
+        setFocusedCardId(section as ModuleId);
+      }, 100);
+      return () => clearTimeout(timer);
+    }
+  }, [params.section, setActiveModule, setViewState, setFocusedCardId]);
+
+  return <Home />;
+}

ee7dedd216df55674a7fd1c873537fe9650e7eba
Deya-Darweesh-ITC <deya@itc-international.net>
2026-04-21 08:53:59 +0300

    config hide next indicator

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['echarts', 'echarts-gl', 'zrender', 'three', 'react-globe.gl', 'maplibre-gl'],
+  devIndicators: false,
 };
+
 
 export default nextConfig;
